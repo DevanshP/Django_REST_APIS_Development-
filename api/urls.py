c8ebd6bf844b9c058ac391ec6ac1a13f61df52d6
Devansh <dvbittu.pandey@gmail.com>
2026-06-22 00:11:30 +0530

    performed crud options using primary key

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
    path('students',views.studentsView),
    path('students/<int:pk>',views.studentDetailview),
-   path('employees/',views.Employees.as_view()) # class based view we need to write as_view() to tell that its a class-based view j
+   path('employees/',views.Employees.as_view()), # class based view we need to write as_view() to tell that its a class-based view j
+   path('employees/<int:pk>/',views.EmployeeDetail.as_view()),
 ]
 
 
